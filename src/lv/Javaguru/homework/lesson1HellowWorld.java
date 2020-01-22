14b7309ec48a6023d71ac9286e4a325fb29dee30
Davis Metriņš <davis@stade.lv>
2020-01-22 21:39:46 +0200

    Sveiciens ar ''pirmo'' programmu!

---
+++ src/lv/Javaguru/homework/lesson1HellowWorld.java
@@ -0,0 +1,9 @@
+package lv.Javaguru.homework;
+
+import java.sql.SQLOutput;
+
+public class lesson1HellowWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Mārtiņ");
+    }
+}
