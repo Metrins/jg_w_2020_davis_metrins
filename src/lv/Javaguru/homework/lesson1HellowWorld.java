3e5e12861abe75e75e32fc99f20ff663f1492f86
Davis Metriņš <davis@stade.lv>
2020-02-03 14:31:58 +0200

    MathOperations_03_02_2020

--- src/lv/Javaguru/homework/lesson1HellowWorld.java
+++ src/lv/Javaguru/homework/lesson1HellowWorld.java
@@ -1,9 +1,27 @@
 package lv.Javaguru.homework;
 
+
 import java.sql.SQLOutput;
 
 public class lesson1HellowWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Mārtiņ");
+
+        int firstNumber = 10;
+        int secondNumber = 5;
+
+        int addition = firstNumber + secondNumber;
+        int subtraction = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        int division = firstNumber / secondNumber;
+
+        System.out.println("First number is 10");
+        System.out.println("Second number is 5");
+
+        System.out.println();
+
+        System.out.println("Addition : " + firstNumber + " + " + secondNumber + " = " + addition);
+        System.out.println("Subtraction : " + firstNumber + " - " + secondNumber + " = " + subtraction);
+        System.out.println("Multiplication : " + firstNumber + " * " + secondNumber + " = " + multiplication);
+        System.out.println("Division : " + firstNumber + " / " + secondNumber + " = " + division);
     }
 }
